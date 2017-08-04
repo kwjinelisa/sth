10e5b78c26a33b1ed925d22832b1533db4fd41c4
kaiwen <kaiwen.jin@qq.com>
2017-08-04 18:00:25 +0800

    session 1.0
    added keepalive

--- Session.java
+++ Session.java
@@ -1,41 +1,82 @@
 package com.coreos.jetcd.concurrency;
 
+import com.coreos.jetcd.Client;
+import com.coreos.jetcd.Lease.KeepAliveListener;
+import com.coreos.jetcd.exception.EtcdExceptionFactory;
+import com.coreos.jetcd.lease.LeaseRevokeResponse;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 
-import com.coreos.jetcd.Client;
-
-/**
- * @author odl
- *
- */
 public class Session {
-	private AtomicReference<Client> etcdclient;
-	private long lease;
-	private int ttl;
-	
-	public Session(long lease, AtomicReference<Client> client,int ttl){
-		this(lease,client);
-		this.ttl=ttl;
-	}
-	
-	public Session(long lease, AtomicReference<Client> client) {
-		this.lease = lease;
-		this.etcdclient = client;
-	}
-	
-	public static Session New(long lease, AtomicReference<Client> client) {
-		return new Session(lease,client);
-	}
-	
-	public static Session New(long lease, AtomicReference<Client> client, int ttl) {
-		return new Session(lease, client, ttl);
-	}
-	
-	public Client getClient(){
-		return etcdclient.get();
-	}
-	
-	public long getLease() {
-		return this.lease;
-	}
+  public static int defaultSessionTTL = 60;
+  
+  private AtomicReference<Client> etcdclient;
+  private long lease;
+  private int ttl;
+  private KeepAliveListener listener;
+  
+  public static Builder newBuilder() {
+    return new Builder();
+  }
+  
+  public static class Builder {
+    private AtomicReference<Client> etcdclient;
+    private long lease = 0L;
+    private int ttl = defaultSessionTTL;
+    
+    public Builder setClient(Client client) {
+      etcdclient = new  AtomicReference<Client>(client);
+      return this;
+    }
+    
+    public Builder setLease(long lease) {
+      this.lease = lease;
+      return this;
+    }
+    
+    public Builder setTtl(int ttl) {
+      this.ttl = ttl;
+      return this;
+    }
+    
+    public Session build() throws InterruptedException, ExecutionException {
+      return new Session(lease, etcdclient, ttl);
+    }
+  }
+  
+  private Session(long lease, AtomicReference<Client> client, int ttl) 
+      throws InterruptedException, ExecutionException {
+    this.etcdclient = client;
+    this.lease = lease;
+    this.ttl = ttl;
+    
+    if (this.lease == 0) {
+      this.lease = client.get().getLeaseClient().grant(this.ttl).get().getID();
+    }
+    listener = client.get().getLeaseClient().keepAlive(this.lease);
+  }
+  
+  public void close() throws Exception {
+    listener.close();
+    CompletableFuture<LeaseRevokeResponse> future = etcdclient.get().getLeaseClient().revoke(lease);
+    try {
+      future.get(ttl,TimeUnit.SECONDS);
+    } catch (TimeoutException e) {
+      // if revoke takes longer than the ttl, lease is expired anyway
+    } catch (InterruptedException | ExecutionException e) {
+      throw EtcdExceptionFactory.newEtcdException("Exception when revoking lease " + lease);
+    }
+  }
+  
+  public Client getClient() {
+    return etcdclient.get();
+  }
+  
+  public long getLease() {
+    return this.lease;
+  }
 }
